8745f485c64b1e0f82c1b537b1c2f969719f1e36
mnaumantahir <mnaumantahir@noreply.github.com>
2021-01-09 12:01:13 +0500

    Adding Splash Screen on Notes App Taking.

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,296 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+// import 'package:quizzler/retry.dart';
+// import 'quiz_brain.dart';
+import 'dart:ui';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(SplashScreen());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Splash Screen',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 6),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => QuizApp())));
+  } //<- Creates an object that fetches an image.
+  // var image = new Image(
+  //     image: AssetImage(
+  //         'asset/quiz.png'),
+  //     height:300);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Container(
+        color: Colors.white,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Image(
+                image: AssetImage(
+                    'Assets/logo.png'),
+                height:300),
+            Text("Muhammad Nouman Tahir",style:TextStyle(fontSize: 20,color: Colors.amber )),
+            SizedBox(height: 10,),
+            CircularProgressIndicator(backgroundColor: Colors.deepPurple)
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
     );
   }
 }
+
+class QuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+            child: QuizPage()
+          // Padding(
+          //   // padding: EdgeInsets.symmetric(horizontal: 10.0),
+          //   child: QuizPage(),
+          // ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage>  {
+
+  // DateTime alert;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   alert = DateTime.now().add(Duration(seconds: 10));
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // quizBrain.shuffle();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text("hello")
+      ],
+    );
+  }
+}
+
+// /*
+// question1: 'You can lead a cow down stairs but not up stairs.', false,
+// question2: 'Approximately one quarter of human bones are in the feet.', true,
+// question3: 'A slug\'s blood is green.', true,
+// */
+// class SecondRoute extends StatelessWidget {
+//   QuizBrain quizBrain;
+//   int correct;
+//   int wrong;
+//   String timer;
+//   List<int> wrong_answers;
+//   List<int> correct_answers;
+//
+//   // final List<int> selected_answer;
+//   SecondRoute(this.quizBrain,this.correct,this.wrong,this.correct_answers,this.wrong_answers,this.timer, {Key key}): super(key: key);
+//   Widget check_timer(bool check){
+//     if (timer=='time' && check==true){
+//       return Container(
+//
+//         child:Text('Time Over',style: TextStyle(fontSize: 30,color: Colors.red,fontWeight: FontWeight.bold),),
+//       );
+//     }
+//     else if (timer=='stop' && check==true){
+//       return Container(
+//
+//         child:Text('Quiz Stopped',style: TextStyle(fontSize: 30,color: Colors.red,fontWeight: FontWeight.bold),),
+//       );
+//     }
+//     else if (timer=='complete' && check==true){
+//       return Container(
+//
+//         child:Text('Quiz Completed',style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),),
+//       );
+//     }
+//     else if(!check && timer!='complete'){
+//       return Container(
+//
+//           child:Column(children: <Widget>[
+//             Text('${10-(correct+wrong)}',style: TextStyle(fontSize: 30,color: Colors.white),),
+//             Text('Unattempt Question',style: TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),)
+//           ],)
+//       );
+//     }
+//     else{
+//       return SizedBox(height: 0,);
+//     }
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey.shade900,
+//       appBar: AppBar(
+//         title: Text("Results"),
+//         leading: new IconButton(
+//             icon: new Icon(Icons.arrow_back),
+//             onPressed: (){ Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => QuizApp()));
+//             }
+//         ),
+//       ),
+//       body: Center(
+//           child: SingleChildScrollView(
+//               child:Column(children: <Widget>[
+//                 Container(
+//                   color: Colors.indigo,
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.start,
+//                     children: <Widget>[
+//                       check_timer(true),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                         children: <Widget>[
+//                           Container(
+//
+//                             child:Text('${correct}',style: TextStyle(fontSize: 30,color: Colors.white),),
+//                           ),
+//                           Container(
+//                             child:Text('${wrong}',style: TextStyle(fontSize: 30,color: Colors.white)),
+//                           ),
+//                         ],
+//                       ),
+//                       Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                         children: <Widget>[
+//                           Container(
+//                             child:Text("True  Answers",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold)),
+//                           ),
+//                           Container(
+//                             child:Text("Wrong Answers",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold)),
+//                           ),
+//                         ],
+//                       ),
+//                       check_timer(false),
+//                       // Container(
+//                       //   child:Text("True  Answers",style: TextStyle(fontSize: 20,color: Colors.white)),
+//                       // ),
+//                       // Container(
+//                       //   child:Text("Unattempt Questions",style: TextStyle(fontSize: 20,color: Colors.white)),
+//                       // ),
+//                     ],),),
+//                 SizedBox(
+//                   height: 20,
+//                 ),
+//                 // SingleChildScrollView(
+//                 Column(
+//                   children: quizBrain.getallquestion(wrong_answers,0),
+//                 ),
+//                 // ),
+//                 // SingleChildScrollView(
+//                 Column(
+//                   children: quizBrain.getallquestion(correct_answers,1),
+//                 )
+//                 // )
+//
+//               ],))
+//
+//       ),
+//     );
+//   }
+// }
+// class score_wrong extends StatefulWidget {
+//   int counter;//if you have multiple values add here
+//   // final List<int> selected_answer;
+//   score_wrong(this.counter, {Key key}): super(key: key);
+//   @override
+//   _PlayButtonState createState() => _PlayButtonState();
+// }
+// class _PlayButtonState extends State<score_wrong> {
+//   IconData icon;
+//
+//   @override
+//   void initState() {
+//     icon = Icons.close;
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: FlatButton(
+//         child: Icon(icon,color: Colors.red,),
+//         onPressed: () {
+//           print(quizBrain.getQuestionText_index(widget.counter));
+//           Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => QuizApp2()));
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// class FAB extends StatelessWidget {
+//   final int id;
+//   final Function(int) onPressed;
+//   // final String buttonText;
+//
+//   const FAB({this.id, this.onPressed});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return  SizedBox(
+//       width: 30,
+//       child:IconButton(
+//
+//         icon: Icon(
+//           Icons.close,
+//           color: Colors.red,
+//
+//         ),
+//         onPressed: () {onPressed(this.id);},
+//       ) ,
+//     ) ;
+//     //   IconButton(
+//     //
+//     //   icon: Icon(
+//     //     Icons.close,
+//     //     color: Colors.red,
+//     //
+//     //   ),
+//     //   onPressed: () {onPressed(this.id);},
+//     // );
+//
+//
+//   }
+//
+// }
